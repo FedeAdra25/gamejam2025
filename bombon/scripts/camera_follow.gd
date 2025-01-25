04a462d3ef0b6d0a9e9a8beacfd340442715877f
fedeadra25 <fedeadra@gmail.com>
2025-01-25 13:21:10 -0300

    Camera movement

--- bombon/scripts/camera_follow.gd
+++ bombon/scripts/camera_follow.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
-
+@onready var player = %Player
+var moveCamera = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +9,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		print(player.position)
+	if moveCamera:
+		global_position += Vector2(delta * 100, 0)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("player")):
+		moveCamera = true

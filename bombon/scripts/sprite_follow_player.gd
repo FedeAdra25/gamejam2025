b4a237a5bac261d766870425ebb7109116d26604
Milagros Andres <andres.mmilagros@gmail.com>
2025-01-26 10:28:54 -0300

    pt el que lea

--- bombon/scripts/sprite_follow_player.gd
+++ bombon/scripts/sprite_follow_player.gd
@@ -16,15 +16,15 @@ func _process(delta: float) -> void:
 	if velocity.x < 0:
 		if !flip_h:
 			flip_h = true
-			play("walk")
+		play("walk")
 	if velocity.y < -0.01:
 		play("up")
-	elif velocity.y > 0.01 && playerPhysics.linear_velocity.y > 0.01:
+	elif velocity.y > 0.01 && playerPhysics.linear_velocity.y > 0.01: # valga la redundancia
 		play("down")
 	elif abs(velocity.x) <= 0.4:
 		play("idle")
 	print("velocity -> ", velocity, "linear velocity -> ", playerPhysics.linear_velocity)
 	speed_scale = playerPhysics.linear_velocity.length() * delta
-	print(speed_scale)
+	#print(speed_scale)
 	global_position = playerPhysics.global_position
 	

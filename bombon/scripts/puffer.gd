44c706dbecbe663ac7aff1169cd9ef8415f3da98
Milagros Andres <andres.mmilagros@gmail.com>
2025-01-26 16:43:18 -0300

    last

--- bombon/scripts/puffer.gd
+++ bombon/scripts/puffer.gd
@@ -10,8 +10,10 @@ var CANTIDAD_DE_VECES_QUE_CORRIO_COLLIDED = 0
 @onready var col_lg = $coliisionlarge
 func _ready() -> void:
 	col_lg.set_disabled(false)
+	sprite.play("default")
 	
 func _on_body_entered(body: Node2D) -> void:
+	#print(body)
 	if(body.has_method("spring") && CANTIDAD_DE_VECES_QUE_CORRIO_COLLIDED == 0):
 		body.spring(global_position)
 		CANTIDAD_DE_VECES_QUE_CORRIO_COLLIDED = CANTIDAD_DE_VECES_QUE_CORRIO_COLLIDED + 1

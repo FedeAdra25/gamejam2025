9ebdbfff24caf955ce5428c999ab6985cdcf0639
fedeadra25 <fedeadra@gmail.com>
2025-01-26 16:27:39 -0300

    FINAL?

--- bombon/scripts/load_scene.gd
+++ bombon/scripts/load_scene.gd
@@ -2,5 +2,10 @@ extends Button
 
 @export var scene : PackedScene
 
+func _ready() -> void:
+	print(scene)
+
 func _on_pressed() -> void:
+	print(scene)
+	scene.instantiate()
 	get_tree().change_scene_to_packed(scene)

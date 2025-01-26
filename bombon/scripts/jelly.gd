437ef896cb0a2e2bfbcdc227a1ff4e7300632420
Milagros Andres <andres.mmilagros@gmail.com>
2025-01-26 13:18:58 -0300

    este es un mal ejemplo de mensaje de commit

--- bombon/scripts/jelly.gd
+++ bombon/scripts/jelly.gd
@@ -3,9 +3,6 @@ extends "res://scripts/enemy_with_path.gd"
 @onready var animatedSprite2d = $AnimatedSprite2D
 
 func _ready() -> void:
-<<<<<<< Updated upstream
-=======
 	#print("done this")
->>>>>>> Stashed changes
 	super._ready()
 	animatedSprite2d.play("default")
